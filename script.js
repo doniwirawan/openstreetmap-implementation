9aba15b09f9b41a17a069547df13dcd52f1bc36f
doniwirawan <doniwirawan166@gmail.com>
2021-09-08 10:57:43 +0800

    make currency for the price

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
+async function main() {
 
-async function main (){
-    
 
     function createMap(elemId, centerLat, centerLng, zoom) {
         var map = new L.Map(elemId);
@@ -23,61 +22,75 @@ async function main (){
         return map;
     }
 
-    var map =  createMap('map', -8.4561, 115.1999, 9);
+    var map = createMap('map', -8.4561, 115.1999, 9);
 
 
-  
 
-    markersLatLngPic = [
-        {
+
+    markersLatLngPic = [{
             point: [-8.5541842, 115.2405467],
-            link:"https://images.unsplash.com/photo-1560103104-4623c14a473b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-            name:"Land 1",
-            desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus turpis lacus, quis gravida orci ultrices vitae. Proin metus mi, lacinia ac nibh eget, gravida sollicitudin magna. Mauris molestie urna leo, quis rhoncus lacus finibus vitae. Vivamus ut est id leo dictum pharetra ac vel nulla. Praesent dignissim mi vel elit finibus, vitae ultrices turpis tristique. ",
-            price: 4500
+            link: "https://images.unsplash.com/photo-1560103104-4623c14a473b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+            name: "Land 1",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus turpis lacus, quis gravida orci ultrices vitae. Proin metus mi, lacinia ac nibh eget, gravida sollicitudin magna. Mauris molestie urna leo, quis rhoncus lacus finibus vitae. Vivamus ut est id leo dictum pharetra ac vel nulla. Praesent dignissim mi vel elit finibus, vitae ultrices turpis tristique. ",
+            price: 4500000,
+            size: 1000,
         },
         {
-            point:[-8.4941595, 115.2355369],
+            point: [-8.4941595, 115.2355369],
             link: "https://images.unsplash.com/photo-1558005530-a7958896ec60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
-            name:"Land 2",
-            desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus turpis lacus, quis gravida orci ultrices vitae. Proin metus mi, lacinia ac nibh eget, gravida sollicitudin magna. Mauris molestie urna leo, quis rhoncus lacus finibus vitae. Vivamus ut est id leo dictum pharetra ac vel nulla. Praesent dignissim mi vel elit finibus, vitae ultrices turpis tristique. ",
-            price: 4500
+            name: "Land 2",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus turpis lacus, quis gravida orci ultrices vitae. Proin metus mi, lacinia ac nibh eget, gravida sollicitudin magna. Mauris molestie urna leo, quis rhoncus lacus finibus vitae. Vivamus ut est id leo dictum pharetra ac vel nulla. Praesent dignissim mi vel elit finibus, vitae ultrices turpis tristique. ",
+            price: 4500000,
+            size: 1000,
         },
         {
             point: [-8.6872526, 115.4328641],
-            link:"https://images.unsplash.com/photo-1557093793-e196ae071479?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-            name:"Land 3",
-            desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus turpis lacus, quis gravida orci ultrices vitae. Proin metus mi, lacinia ac nibh eget, gravida sollicitudin magna. Mauris molestie urna leo, quis rhoncus lacus finibus vitae. Vivamus ut est id leo dictum pharetra ac vel nulla. Praesent dignissim mi vel elit finibus, vitae ultrices turpis tristique. ",
-            price: 4500
+            link: "https://images.unsplash.com/photo-1557093793-e196ae071479?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+            name: "Land 3",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus turpis lacus, quis gravida orci ultrices vitae. Proin metus mi, lacinia ac nibh eget, gravida sollicitudin magna. Mauris molestie urna leo, quis rhoncus lacus finibus vitae. Vivamus ut est id leo dictum pharetra ac vel nulla. Praesent dignissim mi vel elit finibus, vitae ultrices turpis tristique. ",
+            price: 4500000,
+            size: 1000,
         },
         {
             point: [-8.1697395, 114.4250452],
-            link:"https://images.unsplash.com/photo-1501179691627-eeaa65ea017c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-            name:"Land 4",
-            desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus turpis lacus, quis gravida orci ultrices vitae. Proin metus mi, lacinia ac nibh eget, gravida sollicitudin magna. Mauris molestie urna leo, quis rhoncus lacus finibus vitae. Vivamus ut est id leo dictum pharetra ac vel nulla. Praesent dignissim mi vel elit finibus, vitae ultrices turpis tristique. ",
-            price: 4500
+            link: "https://images.unsplash.com/photo-1501179691627-eeaa65ea017c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+            name: "Land 4",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus turpis lacus, quis gravida orci ultrices vitae. Proin metus mi, lacinia ac nibh eget, gravida sollicitudin magna. Mauris molestie urna leo, quis rhoncus lacus finibus vitae. Vivamus ut est id leo dictum pharetra ac vel nulla. Praesent dignissim mi vel elit finibus, vitae ultrices turpis tristique. ",
+            price: 4500000,
+            size: 1000,
         },
-       
-      
+
+
     ];
 
- 
+    function currency(angka) {
+        var reverse = angka.toString().split('').reverse().join(''),
+            ribuan = reverse.match(/\d{1,3}/g);
+        ribuan = ribuan.join('.').split('').reverse().join('');
+        return ribuan;
+    }
 
 
-    function addPopUp(map, latLng,link,name,desc,price) {
+
+    function addPopUp(map, latLng, link, name, desc, price, size) {
         var marker = L.marker(latLng).addTo(map);
         marker.bindPopup(
-        `
-           <div class="card " style="width: 18rem;">
+            `
+           <div class="card mt-3" style="width: 18rem;">
                 <img src="${link}" class="card-img-top" alt="${name}">
                 <div class="card-body p-0 py-2">
                     <div class="d-flex justify-content-between">
-                    <h5 class="card-title">${name}</h5>
-                    <h5 class="text-primary font-weight-bold">IDR.${price}</h5>
+                    <h5 class="card-title font-weight-bold">${name}</h5>
+                    <h5 class="text-warning "><i class="fas fa-money-bill-alt"></i> IDR ${currency(price)}</h5>
+                    </div>
+
+                    <div class="d-flex justify-content-between">
+                    <p class="card-title"><i class="fas fa-ruler-combined mr-2"></i>${size} sqm</p>
+                    
                     </div>
                     
-                    <p class="card-text">${desc}</p>
-                    <a href="#" class="text-white btn btn-primary">Contact Us</a>
+                    <p class="card-text mt-1">${desc}</p>
+                    <a href="https://wa.me/6282146333864?text=I'm%20interested%20to%20buy%20${name}" class="text-white btn btn-success"><i class="fab fa-whatsapp"></i> Contact Us</a>
                 </div>
             </div>
         `).openPopup();
@@ -85,19 +98,17 @@ async function main (){
     }
 
 
- 
+
 
     markersLatLngPic.forEach((latLng) => {
-         console.log(latLng)
-         addPopUp(map, latLng.point,latLng.link,latLng.name, latLng.desc,latLng.price);
+        console.log(latLng)
+        addPopUp(map, latLng.point, latLng.link, latLng.name, latLng.desc, latLng.price, latLng.size);
     });
 
 
 
-   
+
 
 }
 
 main()
-
-
